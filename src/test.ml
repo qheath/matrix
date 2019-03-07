e2e3a79d9a76a6d717debd7f89a41553d8166a37
Quentin Heath <12143333+qheath@users.noreply.github.com>
2019-03-07 02:12:24 +0100

    adapt to size decrease

--- src/test.ml
+++ src/test.ml
@@ -125,7 +125,7 @@ module Line : sig
   type t
 
   val make : w:int -> t
-  val update_copy : density:float -> t -> t -> unit
+  val update_copy : w:int -> density:float -> t -> t -> unit
   val to_image : w:int -> t -> Notty.I.t
 
 end = struct
@@ -135,25 +135,32 @@ end = struct
   let make ~w =
     Array.init w (fun _ -> Cell.make ())
 
-  let update_copy ~density old_line new_line =
+  let update_copy ~w ~density old_line new_line =
     let rand = Random.float 1. in
     let update_copy_cell =
-      if rand<0.02 then
+      if rand<0.015 then
         (fun key value ->
-           let key = if key>0 then key-1 else Array.length new_line - 1 in
-           Array.set new_line key @@ Cell.update ~density value)
-      else if rand<0.04 then
+           if key<w then
+             let key = if key>0 then key-1 else w-1 in
+             Array.set new_line key @@ Cell.update ~density value)
+      else if rand<0.03 then
         (fun key value ->
-           let key = if key<Array.length new_line - 1 then key+1 else 0 in
-           Array.set new_line key @@ Cell.update ~density value)
+           if key<w then
+             let key = if key<w-1 then key+1 else 0 in
+             Array.set new_line key @@ Cell.update ~density value)
       else
         (fun key value ->
-           Array.set new_line key @@ Cell.update ~density value)
+           if key<w then
+             Array.set new_line key @@ Cell.update ~density value)
     in
     Array.iteri update_copy_cell old_line
 
   let to_image ~w line =
-    Notty.I.tabulate w 1 (fun i _ -> Cell.to_image line.(i))
+    Notty.I.tabulate w 1 (fun i _ ->
+      try
+        Cell.to_image line.(i)
+      with _ -> failwith (Format.sprintf "%d/%d" i (Array.length line))
+      )
 
 end
 
@@ -162,7 +169,7 @@ module Matrix : sig
   type t
 
   val make : h:int -> w:int -> t
-  val step : h:int -> density:float -> t -> t
+  val step : w:int -> h:int -> density:float -> t -> t
   val to_image : w:int -> h:int -> t -> Notty.I.t
 
 end = struct
@@ -179,9 +186,9 @@ end = struct
     new_offset = h-1 ;
   }
 
-  let step ~h ~density {lines;old_offset;new_offset} =
+  let step ~w ~h ~density {lines;old_offset;new_offset} =
     Line.update_copy
-      ~density
+      ~w ~density
       (Array.get lines old_offset)
       (Array.get lines new_offset) ;
     {
@@ -197,8 +204,7 @@ end = struct
       let i' = i + old_offset in
       Line.to_image ~w lines.(if i'<h then i' else i'-h)
     in
-    Notty.I.tabulate 1 h aux |> Notty_lwt.eol
-    |> Notty_lwt.eol
+    Notty.I.tabulate 1 h aux |> Notty_lwt.eol |> Notty_lwt.eol
 
 end
 
@@ -328,7 +334,8 @@ end = struct
   let init () =
     let term = Notty_lwt.Term.create () in
     let events = Event.stream_of_term term in
-    let w,h = 90,50 in
+    let w_term,h_term = Notty_lwt.Term.size term in
+    let w,h = w_term/2,h_term in
     let matrix = Matrix.make ~w ~h in
     let frame_rate = UnboundedCounter.make ~lambda:2. 0.05
     and density = BoundedCounter.make ~lambda:0.1 0.01
@@ -353,8 +360,8 @@ end = struct
   let update status =
     let status =
       { status with
-        matrix =
-          Matrix.step ~h:status.h ~density:(BoundedCounter.get status.density) status.matrix ;
+        matrix = Matrix.step ~w:status.w ~h:status.h
+            ~density:(BoundedCounter.get status.density) status.matrix ;
         ts = status.ts + 1 ;
       }
     in
@@ -379,8 +386,8 @@ end = struct
               end
             | _ -> None
           end
+        | `Resize (w,h) -> Some { status with w = w/2 ; h = h }
         | `Paste _ | `Mouse _ -> Some status
-        | `Resize _ -> None
 
   let close {term;_} =
     Notty_lwt.Term.release term ;%lwt
